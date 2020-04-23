348b3d61b1dd6c389b33c0ab9dc769a80751e6cc
gngfang <63154900+gngfang@users.noreply.github.com>
2020-04-23 12:28:15 -0400

    adding object into js

--- main.js
+++ main.js
@@ -1,5 +1,31 @@
 
-let cards = ["queen", "queen", "king", "king"];
+let cards = [
+{
+rank: "queen",
+suit: "hearts",
+cardImage:"images/queen-of-hearts.png"
+},
+{
+rank: "queen",
+suit: "diamonds",
+cardImage:"images/queen-of-diamonds.png"
+
+},
+
+{
+rank: "king",
+suit: "hearts",
+cardImage:"images/king-of-hearts.png"
+
+},
+
+{
+rank: "king",
+suit: "diamonds",
+cardImage:"images/king-of-diamonds.png"
+}
+
+];
 
 let cardsInPlay = [];
 
@@ -7,17 +33,20 @@ function checkForMatch() {
 	if (cardsInPlay[0]=== cardsInPlay[1]) {
 		console.log("you found a match");
 	} else {
-		console.log("sorry try again");
+		alert("sorry try again");
 	}
 };
 
  
 function flipCard(cardId) { 
 
-console.log("User flipped " + cards[cardId]);
- cardsInPlay.push(cards[cardId]);
+console.log("User flipped " + cards[cardId].rank);
+ cardsInPlay.push(cards[cardId].rank);
+ console.log(cards[cardId].cardImage)
+ console.log(cards[cardId].suit)
 if (cardsInPlay.length === 2) {
 	checkForMatch();
+	cardsInPlay = [];
 }
 };
 
