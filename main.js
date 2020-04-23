8b0c065efc2276aa2ef547dc9bf0a3e94cc91ef4
gngfang <63154900+gngfang@users.noreply.github.com>
2020-04-23 03:50:28 -0400

    create array

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
-let cardOne = "Queen";
-let cardTwo = "Queen";
-let cardThree = "King";
-let cardFour = "King";
 
-console.log("User Flipped" + cardThree);
+let cards = ["queen", "queen", "king", "king"];
+
+let cardsInPlay = [];
+
+let cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
+
+console.log("User flipped" + " " + cardOne);
+let cardTwo = cards[2];
+
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped" + " " + cardTwo);
+ 
+if (cardsInPlay.length===2){
+if (cardsInPlay[0]===cardsInPlay[1]){
+	alert("You Found a match");
+
+
+}else {
+	alert("Sorry,try again")
+}
+
+}
